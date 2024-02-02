e6726c1b0b4e3acf4dc4fb332b8b1df817943721
Demeter Áron <demeter.aron0614@gmail.com>
2024-02-02 06:58:07 +0100

    Asm Instruction enum done

---
+++ src/helium/instructions.rs
@@ -0,0 +1,48 @@
+pub enum AsmInstruction {
+    Halt,
+    NoOperation,
+    Reset,
+
+    CallInterrupt,
+    ReturnInterrupt,
+    WaitUntilInterrupt,
+    SetInterruptAddress,
+
+    Move,
+
+    Store, StoreProgramMemory,
+
+    Load, LoadProgramMemory,
+
+    Push, Pop,
+
+    Add, AddWithCarry,
+    Sub, SubWithCarry,
+
+    Negative,
+
+    Increment, Decrement,
+
+    And, Or, Xor, Not,
+
+    ShiftLeft, ShiftRight,
+
+    SetBit, BitCheck,
+
+    Compare, CompareSigned,
+
+    Jump, Call, Return,
+
+    JumpEquals,         CallEquals,         ReturnEquals,
+    JumpNotEquals,      CallNotEquals,      ReturnNotEquals,
+    JumpLessThan,       CallLessThan,       ReturnLessThan,
+    JumpLessThanEq,     CallLessThanEq,     ReturnLessThanEq,
+    JumpNotLessThan,    CallNotLessThan,    ReturnNotLessThan,
+    JumpGreaterThan,    CallGreaterThan,    ReturnGreaterThan,
+    JumpGreaterThanEq,  CallGreaterThanEq,  ReturnGreaterThanEq,
+    JumpNotGreaterThan, CallNotGreaterThan, ReturnNotGreaterThan,
+
+    JumpZero, CallZero, ReturnZero,
+    JumpOverflow, CallOverflow, ReturnOverflow,
+    JumpCarry, CallCarry, ReturnCarry
+}
