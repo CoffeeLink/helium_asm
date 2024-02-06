62f5805d322e84c2d0a8a1b01dc8f6630b8ddaeb
Demeter Áron <demeter.aron0614@gmail.com>
2024-02-06 20:47:08 +0100

    Include kinda done Note: does not detect constant collisions.

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,37 @@ use helium_asm::Config;
 use helium_asm::helium::errors::Error;
 use helium_asm::helium::lexer::Lexer;
 use helium_asm::helium::parser::{Parser};
+/*
+TODO: Refactor everything to as readable as possible. (priority: Less Tabs)
+
+Features to finish:
+    - Include system
+    - const pre_load constants with a const storage
+ */
 
 fn main() {
     let conf = Config::from_args()
         .expect("Epic Fail");
-    let file_contents = read_to_string(conf.get_file())
+    let name = conf.get_file();
+    let file_contents = read_to_string(name.clone())
         .expect("Failed to read file.");
 
-    let tokens = match Lexer::new(&file_contents)
+    let tokens = Lexer::new(&file_contents)
         .lex()
-    {
-        Ok(tokens) => tokens,
-        Err(errors) => {
-            display_errors(errors);
-            exit(1);
-        }
-    };
+        .unwrap_or_else(|e|{
+            display_errors_and_exit(e)
+    });
 
-    let parsed = match Parser::new(&tokens).parse(None) {
-        Ok(t) => {t}
-        Err(e) => {
-            display_errors(e);
-            exit(1);
-        }
-    };
+    let parsed = Parser::new(&tokens, name)
+        .parse(None)
+        .unwrap_or_else(|e|{
+            display_errors_and_exit(e)
+        });
+
     println!("{:?}", parsed)
 }
 
-fn display_errors(errors : Vec<Error>) {
+fn display_errors_and_exit(errors : Vec<Error>) -> ! {
     println!("Errors : {:?}", errors);
+    exit(1)
 }
